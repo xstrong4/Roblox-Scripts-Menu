bcaae32af9b11af8b426b8b4b05aea1659a6d956
xStrong <83123759+xSTronG30@users.noreply.github.com>
2023-03-26 20:19:03 +0200

    Update xsguiv2.lua

--- xsguiv2.lua
+++ xsguiv2.lua
@@ -38,8 +38,6 @@ end)
 local TabBox = Tabs.Main:AddLeftTabbox() -- Add Tabbox on left side
 
 local Tab1 = TabBox:AddTab('Misc')
-local Tab2 = TabBox:AddTab('Misc')
-
 
 Tab1:AddSlider('WalkSpeed', {
     Text = 'Set WalkSpeed',
@@ -127,6 +125,42 @@ coroutine.wrap(setInfinityJumpButton)()
 --
 		
 end)
+
+Tap1:AddButton('Airwalk', function()
+-----
+		local crtl = false
+
+		mouse = game.Players.LocalPlayer:GetMouse()
+
+
+		local air = Instance.new("Part", workspace)
+		air.Size = Vector3.new(7, 2, 3)
+		air.CFrame = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame + Vector3.new(0, -4, 0)
+		air.Transparency = 1
+		air.Anchored = true
+		air.Name = "Airwalk"
+
+
+		mouse.KeyDown:Connect(function(key)
+		   if key == "2" then
+		       air.Size = Vector3.new(4, -0.5, 3)
+		   end
+		end)
+
+		mouse.KeyUp:Connect(function(key)
+		   if key == "2" then
+		       air.Size = Vector3.new(7, 2, 3)
+		   end
+		end)
+
+
+		for i = 1, math.huge do
+		   air.CFrame = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame + Vector3.new(0, -4, 0)
+		   wait(0.01)
+		end
+----
+end)
+
 -- Groupbox and Tabbox inherit the same functions
 -- except Tabboxes you have to call the functions on a tab (Tabbox:AddTab(name))
 local DevLeftBox = Tabs.Dev:AddLeftGroupbox('Groupbox')
